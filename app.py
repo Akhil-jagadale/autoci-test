ad3ca4a3ca0e59417c79904319af3f5e07cfaa15
Akhilesh <141340804+Akhil-jagadale@users.noreply.github.com>
2026-04-08 15:42:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "AutoCI deployed successfully 🚀"
+    return "AutoCI v2 deployed 🔥"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
